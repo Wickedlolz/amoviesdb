394b9991aefb6b96a55287c34943e042e7d54727
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-28 13:27:37 +0300

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Container } from 'react-bootstrap';
+import NavBar from './components/NavBar/NavBar';
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <Container>
+            <NavBar />
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,47 @@
+import {
+    Navbar,
+    Nav,
+    Container,
+    Form,
+    FormControl,
+    Button,
+} from 'react-bootstrap';
+
+function NavBar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand>AMoviesDB</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link>Sign In</Nav.Link>
+                        <Nav.Link>Sign Up</Nav.Link>
+                    </Nav>
+                    <Navbar.Text>
+                        Signed in as: <a href="#/">Viktor Dimitrov</a>
+                    </Navbar.Text>
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            name="search"
+                            aria-label="Search"
+                        />
+                        <Button type="submit" variant="outline-success">
+                            Search
+                        </Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
