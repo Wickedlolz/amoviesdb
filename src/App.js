c7cad7ef3fc96bfdcb36555d6c568136b816c487
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-28 13:51:39 +0300

    add MovieDetails

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Container } from 'react-bootstrap';
 import NavBar from './components/NavBar/NavBar';
 import Catalog from './components/Catalog/Catalog';
+import MovieDetails from './components/Catalog/MovieDetails';
 
 function App() {
     return (
         <Container>
             <NavBar />
-            <Catalog />
+            {/* <Catalog /> */}
+            <MovieDetails />
         </Container>
     );
 }
