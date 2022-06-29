82b301f6b8b20bb527792032ee375baac9b585b7
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-29 21:51:33 +0300

    move mockData in Catalog to state

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -1,60 +1,62 @@
+import { useState } from 'react';
 import { Row } from 'react-bootstrap';
 import MovieList from '../MovieList/MovieList';
 import Pagination from '../Pagination/Pagination';
 
-const mockData = [
-    {
-        id: 1,
-        title: 'Avengers',
-        imageUrl: 'https://source.unsplash.com/random',
-    },
-    {
-        id: 2,
-        title: 'Doctor Strange',
-        imageUrl: 'https://source.unsplash.com/random',
-    },
-    {
-        id: 3,
-        title: 'Spider-Man No Way Home',
-        imageUrl: 'https://source.unsplash.com/random',
-    },
-    {
-        id: 4,
-        title: 'Venom',
-        imageUrl: 'https://source.unsplash.com/random',
-    },
-    {
-        id: 5,
-        title: 'Batman',
-        imageUrl: 'https://source.unsplash.com/random',
-    },
-    {
-        id: 6,
-        title: 'League of Justice',
-        imageUrl: 'https://source.unsplash.com/random',
-    },
-    {
-        id: 7,
-        title: 'Morbius',
-        imageUrl: 'https://source.unsplash.com/random',
-    },
-    {
-        id: 9,
-        title: 'Iron Man',
-        imageUrl: 'https://source.unsplash.com/random',
-    },
-];
 function Catalog() {
+    const [movies, setMovies] = useState([
+        {
+            id: 1,
+            title: 'Avengers',
+            imageUrl: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 2,
+            title: 'Doctor Strange',
+            imageUrl: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 3,
+            title: 'Spider-Man No Way Home',
+            imageUrl: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 4,
+            title: 'Venom',
+            imageUrl: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 5,
+            title: 'Batman',
+            imageUrl: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 6,
+            title: 'League of Justice',
+            imageUrl: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 7,
+            title: 'Morbius',
+            imageUrl: 'https://source.unsplash.com/random',
+        },
+        {
+            id: 9,
+            title: 'Iron Man',
+            imageUrl: 'https://source.unsplash.com/random',
+        },
+    ]);
+
     return (
         <>
             <div className="p-5 text-center bg-light">
                 <h1 className="mb-3">Welcome to AMoviesDB</h1>
                 <h4 className="mb-3">Subheading</h4>
             </div>
-            {mockData.length > 0 ? (
+            {movies.length > 0 ? (
                 <>
                     <Row xs={1} md={4} className="g-3">
-                        <MovieList movies={mockData} />
+                        <MovieList movies={movies} />
                     </Row>
                     <Pagination />
                 </>
