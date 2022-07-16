6f424f35adf4de7450e1dcba4d67c54c45b4fe73
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-07-16 20:49:06 +0300

    add custom error boundry

--- src/App.js
+++ src/App.js
@@ -19,42 +19,48 @@ import Notification from './components/Common/Notification';
 import MyMovies from './components/MyMovies/MyMovies';
 import AboutUs from './components/AboutUs/AboutUs';
 import Home from './components/Home/Home';
+import CustomErrorBoundry from './components/CustomErrorBoundry/CustomErrorBoundry';
 
 function App() {
     return (
-        <AuthProvider>
-            <NotificationProvider>
-                <Container>
-                    <NavBar />
-                    <Notification />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/catalog" element={<Catalog />} />
-                        <Route path="/signin" element={<SignIn />} />
-                        <Route path="/signup" element={<SignUp />} />
-                        <Route path="/signout" element={<SignOut />} />
-                        <Route path="/profile/:userId" element={<Profile />} />
-                        <Route
-                            path="/profile/edit/:userId"
-                            element={<EditProfile />}
-                        />
-                        <Route
-                            path="/movie/:movieId"
-                            element={<MovieDetails />}
-                        />
-                        <Route
-                            path="/my-movies/:userId"
-                            element={<MyMovies />}
-                        />
-                        <Route path="/create" element={<Create />} />
-                        <Route path="/edit/:movieId" element={<Edit />} />
-                        <Route path="/about" element={<AboutUs />} />
-                        <Route path="/*" element={<NotFound />} />
-                    </Routes>
-                    <Footer />
-                </Container>
-            </NotificationProvider>
-        </AuthProvider>
+        <CustomErrorBoundry>
+            <AuthProvider>
+                <NotificationProvider>
+                    <Container>
+                        <NavBar />
+                        <Notification />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/catalog" element={<Catalog />} />
+                            <Route path="/signin" element={<SignIn />} />
+                            <Route path="/signup" element={<SignUp />} />
+                            <Route path="/signout" element={<SignOut />} />
+                            <Route
+                                path="/profile/:userId"
+                                element={<Profile />}
+                            />
+                            <Route
+                                path="/profile/edit/:userId"
+                                element={<EditProfile />}
+                            />
+                            <Route
+                                path="/movie/:movieId"
+                                element={<MovieDetails />}
+                            />
+                            <Route
+                                path="/my-movies/:userId"
+                                element={<MyMovies />}
+                            />
+                            <Route path="/create" element={<Create />} />
+                            <Route path="/edit/:movieId" element={<Edit />} />
+                            <Route path="/about" element={<AboutUs />} />
+                            <Route path="/*" element={<NotFound />} />
+                        </Routes>
+                        <Footer />
+                    </Container>
+                </NotificationProvider>
+            </AuthProvider>
+        </CustomErrorBoundry>
     );
 }
 
---
+++ src/components/CustomErrorBoundry/CustomErrorBoundry.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class CustomErrorBoundry extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false,
+        };
+    }
+
+    static getDerivedStateFromError(error) {
+        return {
+            hasError: true,
+        };
+    }
+
+    componentDidCatch(error) {
+        console.log('Error from ComponentDidCatch: ', error);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div className="d-flex align-items-center justify-content-center vh-100">
+                    <div className="text-center">
+                        <p className="fs-3">
+                            {' '}
+                            <span className="text-danger">Opps!</span> something
+                            went wrong.
+                        </p>
+                        <p className="lead">Try to reload page.</p>
+                    </div>
+                </div>
+            );
+        }
+
+        return this.props.children;
+    }
+}
+
+export default CustomErrorBoundry;
